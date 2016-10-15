b619281b75d33d8c27160e2792e587dd4e9e2fc5
Connor1996 <zbk602423539@gmail.com>
2016-10-15 18:14:15 +0800

    Update ormlite.h

--- src/ormlite.h
+++ src/ormlite.h
@@ -4,19 +4,22 @@
 #include "include/sqlite3.h"
 
 #include <string>
-#include <set>
+#include <vector>
 #include <exception>
 
+// 序列化所用的分隔符
+#define SEPARATOR ','
+
 #define ORMAP(_CLASS_NAME_, ...)                                \
-template <typename VISITOR>                                     \
 private:                                                        \
 friend class ORMLite::ORMapper<_CLASS_NAME_>;                   \
-void Accept(VISTIOR &visitor)                                   \
+template <typename VISITOR>                                     \
+void _Accept(const VISTIOR &visitor)                            \
 {                                                               \
     visitor.visit(__VA_ARGS__);                                 \
 }                                                               \
 constexpr static const std::string _CLASSNAME = #_CLASS_NAME_;  \
-constexpr static const std::string _FIELDNAMES = #__VA_ARGS__
+constexpr static const std::string _FIELDSNAME = #__VA_ARGS__
 
 
 namespace ORMLite {
@@ -27,30 +30,82 @@ class ORMapper
 public:
     ORMapper(const std::string &dbName)
         : _dbName(dbName), _tableName(T::_CLASSNAME),
-          _fieldNames(_split(T::_FILEDNAMES))
+          _fieldsName(ORMLite_Impl::_Split(T::_FILEDSNAME))
     { }
 
     bool CreateTable()
     {
         return _HandleException([&] (ORMLite_Impl::SQLConnector &connector)
         {
+            // 创建model类，使用访问者访问获得类中元信息
+            const T modelObject {};
+            ORMLite_Impl::TypeVisitor vistor;
+            modelObject._Accept(vistor);
 
+            auto fieldsType = ORMLite_Impl::_Split(vistor.GetSerializedStr());
+            std::vector::size_type indexOfFields = 0;
+
+            auto FieldsStr = _fieldsNames[0] + " " +
+                    fieldsType[0] + " PRIMARY KEY NOT NULL,";
+            while (indexOfFields++ < _fieldsName.size())
+            {
+                FieldsStr += _fieldsName[indexOfFields] + " " +
+                        _fieldsType[indexOfFields] + ",";
+            }
+            auto commandStr = "CREATE TABLE " + _tableName +
+                    " IF NOT EXISTS" + "(" + FieldsStr + ");";
+
+            connector.Excute(commandStr);
         })
     }
 
     bool DropTable()
     {
-
+        return _HandleException([&] (ORMLite_Impl::SQLConnector &connector)
+        {
+            connector.Excute("DROP TABLE " + _tableName + ";");
+        })
     }
 
-    bool Select()
+    bool Select(const std::string &property)
     {
+        return _HandleException([&] (ORMlite_Impl::SQLConnector &connector)
+        {
 
+        })
     }
 
-    bool Delete()
+    bool Insert(const )
+    // 读入指定的对象，根据该对象的主键删除整行
+    bool Delete(const T &modelObject)
     {
+        return _HandleException([&] (ORMLite_Impl::SQLConnector &connector)
+        {
+            ORMLite_Impl::ReaderVisitor visitor;
+            modelObject._Accept(visitor);
 
+            // 格式如 "property = value"
+            auto KeyValueStr = _fieldNames[0] + "=" + visitor.GetSerializedStr();
+            connector.Excute("DELETE FROM " + _tableName + " " +
+                             "WHERE " + formatStr + ";");
+        })
+    }
+
+    bool Update(const T &modelObject)
+    {
+        return _HandleException([&] (ORMLite::Impl::SQLConnector &connector)
+        {
+
+        })
+    }
+
+    // 删除所有行
+    bool DeleteAll()
+    {
+        return _HandleException([&] (ORMLite_Impl::SQLConnector &connector)
+        {
+            connector.Excute("DELETE FROM " + _tableName + ";");
+        })
     }
 
     bool Update()
@@ -61,7 +116,7 @@ public:
 private:
     const std::string _dbName;
     const std::string _tableName;
-    const std::set<std::string> _fieldNames;
+    const std::vector<std::string> _fieldNames;
     std::string _errorMessage;
 
     bool _HandleException(std::function<void (ORMLite_Impl::SQLConnector &)> fn)
@@ -79,9 +134,9 @@ private:
         }
     }
 
-    static std::set<std::string> _Split(std::string str)
+    static std::vector<std::string> _Split(std::string str)
     {
-        std::set<std::string> result;
+        std::vector<std::string> result;
         std::string tempStr = "";
         // 扩展字符串方便操作
         str += ',';
@@ -90,8 +145,9 @@ private:
         {
             switch (ch)
             {
+            case SEPARATOR:
             case ',':
-                result.insert(tempStr);
+                result.push_back(tempStr);
                 tempStr.clear();
                 break;
             case ' ':
@@ -131,6 +187,79 @@ public:
 
 private:
     sqlite3* _database;
+};
+
+class TypeVisitor
+{
+public:
+    template <typename ...Args>
+    inline void Visit(Args & ...args)
+    {
+        return _Visit(args...);
+    }
+
+    std::string GetSerializedStr() const { return _serialized; }
+private:
+    std::string _serializedStr;
+
+    template <typename First, typename ...Args>
+    inline void _Visit(First& property, Args & ...args)
+    {
+        _Visit(property);
+        _Visit(args);
+    }
+
+    inline void _Visit(const int &property)
+    {
+        _serializedStr += "INT";
+        _serializedStr += SEPARATOR;
+    }
+    inline void _Visit(const double &property)
+    {
+        _serializedStr += "REAL";
+        _serializedStr += SEPARATOR;
+    }
+    inline void _Visit(const std::string &property)
+    {
+        _serializedStr += "TEXT";
+        _serializedStr += SEPARATOR;
+    }
+};
+
+class ReaderVisitor
+{
+public:
+    template <typename ...Args>
+    inline void Visit(Args & ...args)
+    {
+        return _Visit(args...);
+    }
+
+    std::string GetSerializedStr() const { return _serialized; }
+private:
+    std::tuple _serializedStr;
+
+    template <typename First, typename ...Args>
+    inline void _Visit(First &property, Args &...args)
+    {
+        _Visit(property);
+        _Visit(args);
+    }
+
+    inline void _Visit(const int &property)
+    {
+        _serializedStr = std::to_string(property);
+    }
+    inline void _Visit(const double &property)
+    {
+        _serializedStr += std::to_string(property);
+    }
+    inline void _Visit(const std::string &property)
+    {
+        _serializedStr += "'" + property + "'";
+    }
+
+    _
 };
 }
 #endif // ORMLITE_H
