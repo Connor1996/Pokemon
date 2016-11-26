872c1f7389a0628b034606c776317bdc94b90c7e
Connor1996 <zbk2008@vip.qq.com>
2016-11-26 16:50:35 +0800

    Add socket-server multi-thread support

--- Socket/client/main.cpp
+++ Socket/client/main.cpp
@@ -5,8 +5,10 @@
 int main(int argc, char *argv[])
 {
     std::cout << "this is client" << std::endl;
-    try{
-        Socket::Client client;
+    std::string name = (argc == 1) ? "UNKNOWN" : argv[1];
+    try
+    {
+        Socket::Client client = Socket::Client(name);
     } catch (std::exception e){
         std::cout << e.what() << std::endl;
     }
--- Socket/socket.h
+++ Socket/socket.h
@@ -1,6 +1,13 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+
 #include <iostream>
+#include <vector>
+#include <string>
+
+#include <thread>
+#include <mutex>
+#include <chrono>
 
 #define CLIENT_PORT 5150
 #define SERVER_PORT 2222
@@ -14,6 +21,7 @@ namespace Socket
 {
 using std::cout;
 using std::endl;
+using std::string;
 
 class Socket{
 public:
@@ -35,10 +43,8 @@ protected:
 
 class Client : Socket{
 public:
-    Client() : Client(CLIENT_PORT)
-    { }
-
-    Client(int port) : _port(port)
+    Client() { }
+    Client(string name) : _name(name)
     {
         std::cout << "begin..." << std::endl;
         _connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -62,15 +68,25 @@ public:
             throw std::runtime_error("Second parrameter does not contain valid ipaddress");
         }
         // 向服务器发出连接请求
-        if (connect(_connectSocket, reinterpret_cast<SOCKADDR *>(&_serverAddr),
-                    sizeof(_serverAddr))== -1)
+        auto trys = 10;
+        while(trys--)
         {
-             closesocket(_connectSocket);
-             throw std::runtime_error("Unable to connect to server");
+            if (connect(_connectSocket, reinterpret_cast<SOCKADDR *>(&_serverAddr),
+                        sizeof(_serverAddr))== -1)
+            {
+                 cout << "Unable to connect to server... " << std::to_string(10 - trys) << " trys" << endl;
+                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                 continue;
+            }
+            cout << "Initial success" << endl;
+            while(true)
+            {
+                getchar();
+                send();
+            }
         }
-        cout << "Initial success" << endl;
-        send();
-        cout << "send complete" << endl;
+        closesocket(_connectSocket);
+        throw std::runtime_error("Unable to connect to server, Please try later");
     }
 
     ~Client()
@@ -82,21 +98,22 @@ public:
 protected:
     SOCKET _connectSocket;
     SOCKADDR_IN _serverAddr;
-    int _port;
+    string _name;
 
     void send()
     {
         #define DEFAULT_BUFLEN 200
         int recvbuflen = DEFAULT_BUFLEN;
-        char *sendbuf = const_cast<char *>("this is a test");
+        string sendbuf = "this is a test, by: " + _name;
         char recvbuf[DEFAULT_BUFLEN];
 
-        int iResult = ::send(_connectSocket, sendbuf, (int)strlen(sendbuf), 0);
+        int iResult = ::send(_connectSocket, sendbuf.c_str(), sendbuf.length(), 0);
         if (iResult == SOCKET_ERROR)
         {
             closesocket(_connectSocket);
             throw std::runtime_error("Failed at send message");
         }
+        cout << "send complete" << endl;
     }
 };
 
@@ -105,52 +122,71 @@ class Server : Socket
 public:
     Server()
     {
-        listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        _listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (_listeningSocket == -1)
+            throw std::runtime_error("Cannot create listening socket");
 
         _serverAddr.sin_family = AF_INET;
         _serverAddr.sin_port = htons(SERVER_PORT);
         _serverAddr.sin_addr.s_addr = INADDR_ANY;
 
         // 绑定监听端口
-        if (bind(listeningSocket, reinterpret_cast<SOCKADDR*>(&_serverAddr),
+        if (bind(_listeningSocket, reinterpret_cast<SOCKADDR*>(&_serverAddr),
                  sizeof(_serverAddr)) == -1)
         {
-            closesocket(listeningSocket);
+            closesocket(_listeningSocket);
             throw std::runtime_error("Failed at bind");
         }
 
         // 开始监听，指定监听队列的大小
-        if (listen(listeningSocket, 5))
+        if (listen(_listeningSocket, 5))
         {
-            closesocket(listeningSocket);
+            closesocket(_listeningSocket);
             throw std::runtime_error("Failed at listen");
         }
         std::cout << "Wait for connection..." << std::endl;
         // 不断等待客户端请求的到来
         while (1)
         {
-            SOCKET newConnection = accept(listeningSocket, NULL, NULL);
-            std::cout << "new come" << std::endl;
-            if (newConnection < 0)
+            SOCKET newConnection = accept(_listeningSocket, NULL, NULL);
+            _socketThreads.emplace_back(std::thread([this] (SOCKET &&connection)
             {
-                cout << "Failed at accept" << endl;
-                continue;
-            }
-            char recvBuf[200];
-            recv(newConnection, recvBuf, 200, 0);
+                char recvBuf[200];
+                if (connection < 0)
+                {
+                    cout << "Failed at accept" << endl;
+                }
+                else
+                {
+                    cout << "New come, now " << _socketThreads.size() << " connections in total" << endl;
+
+                    while(true)
+                    {
+                        if (recv(connection, recvBuf, 200, 0) == -1)
+                        {
+                            shutdown(connection, SD_BOTH);
+                            closesocket(connection);
+                            break;
+                        }
+                        cout << "message: " << recvBuf << endl;
+                    }
+                }
+            }, std::move(newConnection)));
         }
     }
 
     ~Server()
     {
-        shutdown(listeningSocket, SD_BOTH);
-        closesocket(listeningSocket);
+        shutdown(_listeningSocket, SD_BOTH);
+        closesocket(_listeningSocket);
     }
 
 protected:
-    SOCKET listeningSocket;
+    SOCKET _listeningSocket;
 
     SOCKADDR_IN _serverAddr;
     SOCKADDR_IN _clientAddr;
+
+    std::vector<std::thread> _socketThreads;
 };
 }
